5b40936b74d7745c11d9db6de0feb7ed6ce63399
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2024-10-07 21:36:01 -0300

    feat: сброс индекса слайдов

--- frontend/src/shared/ui/to-return/ui/to-return.tsx
+++ frontend/src/shared/ui/to-return/ui/to-return.tsx
@@ -1,3 +1,5 @@
+import { useAppDispatch } from "@/app/hooks@depercated";
+import { setIndexCurrentSlide } from "@/features/quiz-navigation/model/slice";
 import { Link } from "react-router-dom";
 
 interface ToReturnProps {
@@ -6,9 +8,21 @@ interface ToReturnProps {
 }
 
 const ToReturn: React.FC<ToReturnProps> = ({ children, to }) => {
+    const dispatch = useAppDispatch();
+
+    const handleResetIndex = () => {
+        dispatch(
+            setIndexCurrentSlide({
+                indexCurrentSlide: 0,
+            }),
+        );
+    };
+
     return (
         <>
-            <Link to={to}>{children}</Link>
+            <Link to={to} onClick={() => handleResetIndex()}>
+                {children}
+            </Link>
         </>
     );
 };
