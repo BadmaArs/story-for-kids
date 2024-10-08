2f69bed4e25b94bd6dd5fb960477fe6261f39614
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2024-10-08 09:30:19 -0300

    feat: кнопка возврата на главную страницу

--- frontend/src/pages/lesson/lesson.tsx
+++ frontend/src/pages/lesson/lesson.tsx
@@ -1,8 +1,14 @@
+import { ToReturn } from "@/shared/ui/to-return";
 import { LessonList } from "@/widgets/lesson-list";
 
 const Lesson = () => {
     return (
         <div className="">
+            <ToReturn to="/">
+                <button className="btn mt-5 ml-3 md:ml-20 md:mt-10">
+                    Вернуться
+                </button>
+            </ToReturn>
             <LessonList />
         </div>
     );
