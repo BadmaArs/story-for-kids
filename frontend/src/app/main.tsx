0ce285609039f1093950136baa2a346cc59ac0c9
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2024-10-17 13:36:49 -0300

    feat: redux-persist

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,13 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { router } from "./router";
-import { Provider } from "react-redux";
-import { appStore } from "./store";
+import { appStore, persistor } from "./store";
 import "./index.css";
+import { Loader } from "@/shared/ui/loader";
 
 createRoot(document.getElementById("root")!).render(
     <Provider store={appStore}>
+        <PersistGate loading={<Loader />} persistor={persistor}>
         <RouterProvider router={router} />
+        </PersistGate>
     </Provider>,
 );
--- frontend/src/app/store/store.ts
+++ frontend/src/app/store/store.ts
@@ -1,25 +1,52 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { booksApi } from "@/entities/books";
 import { lessonsApi } from "@/entities/topic/api/lessons-api";
 import { slidesApi } from "@/pages/quiz";
 import { rootReducer } from "./rootReducer";
 
+const persistConfig = {
+    key: "root",
+    storage,
+    blackList: [
+        lessonsApi.reducerPath,
+        booksApi.reducerPath,
+        slidesApi.reducerPath,
+    ],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export function makeStore() {
     const store = configureStore({
-        reducer: rootReducer,
+        reducer: persistedReducer,
         middleware: (getDefaultMiddleware) =>
-            getDefaultMiddleware()
+            getDefaultMiddleware({
+                serializableCheck: {
+                  ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+                },
+              })
                 .concat(lessonsApi.middleware)
                 .concat(booksApi.middleware)
                 .concat(slidesApi.middleware),
     });
     setupListeners(store.dispatch);
-
     return store;
 }
 
 export const appStore = makeStore();
+export const persistor = persistStore(appStore);
 
 export type RootState = ReturnType<typeof appStore.getState>;
 
